8eb31c27cb0cb147c1324a679f56aafadd2aa86d
raxmon04 <ramonpeter6@gmail.com>
2025-11-19 16:10:39 +0100

    047 Amazon Price Tracker finished

--- 047_AmazonPriceTracker/main.py
+++ 047_AmazonPriceTracker/main.py
@@ -4,28 +4,49 @@ import smtplib
 # Add the os and dotenv modules
 import os
 from dotenv import load_dotenv
+import re
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Practice
-url = "https://appbrewery.github.io/instant_pot/"
+# url = "https://appbrewery.github.io/instant_pot/"
 # Live Site
-# url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+url = "https://www.amazon.de/-/en/Philips-OneBlade-Original-Blades-Replacement/dp/B0BS747TYL/ref=zg_bs_c_drugstore_d_sccl_3/259-1767365-8564830"
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+    "Accept-Encoding": "gzip, deflate, br, zstd", 
+    "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7", 
+    "Priority": "u=0, i", 
+    "Sec-Ch-Ua": "\"Chromium\";v=\"142\", \"Google Chrome\";v=\"142\", \"Not_A Brand\";v=\"99\"", 
+    "Sec-Ch-Ua-Mobile": "?0", 
+    "Sec-Ch-Ua-Platform": "\"Windows\"", 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "cross-site", 
+    "Sec-Fetch-User": "?1", 
+    "Upgrade-Insecure-Requests": "1", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36", 
+}
 
-response = requests.get(url)
+response = requests.get(url, headers=header)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find the HTML element that contains the price
 price = soup.find(class_="a-offscreen").get_text()
+# print(price)
 
-# Remove the dollar sign using split
-price_without_currency = price.split("$")[1]
+# REGEX: finde die erste Zahl mit zwei Dezimalstellen
+match = re.search(r"(\d+[.,]\d{2})", price)
 
-# Convert to floating point number
-price_as_float = float(price_without_currency)
-# print(price_as_float)
+if not match:
+    raise ValueError("Kein gültiger Preis gefunden!")
+
+number_str = match.group(1).replace(",", ".")
+price_as_float = float(number_str)
+
+# print("Preis als Float:", price_as_float)
 
 # ====================== Send an Email ===========================
 
